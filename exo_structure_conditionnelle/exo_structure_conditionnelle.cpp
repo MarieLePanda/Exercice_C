92eda8987de65b6a22d079ae16b9690b55b6eafe
Marie le Panda <lucas.girardin@outlook.com>
2013-05-20 00:02:17 +0200

    Ex5

--- exo_structure_conditionnelle/exo_structure_conditionnelle.cpp
+++ exo_structure_conditionnelle/exo_structure_conditionnelle.cpp
@@ -9,7 +9,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	
 	/*
 		Exercice 2 : Multiple
-		Ecrire un programme qui affiche si le nombre saisi par l'utilisateur est un multiple de 3
+		Ecrire un programme qui affiche si le nombre saisi par l'utilisateur est un multiple de 3.
 	*/
 	
 	printf("--------------------------------------------\n");
@@ -32,10 +32,10 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	/*
 		Exercice 3 : Prime
-		La soiciété "SIMPA" prévoit de donner à chacun de ses employés une prime de fin d'année à hauteur de 3% du salaire net.
-		Cette prime sera majorée de 100€ pour les salariés dont l'ancienneté est supérieure à 5 ans.
-		Ecrire l'algorithme qui permet de saisir le salaire et l'ancienneté puis de calculer et d'afficher le montant de la prime 
-		d'un salarié.
+		La soiciete "SIMPA" prevoit de donner à chacun de ses employes une prime de fin d'annee à hauteur de 3% du salaire net.
+		Cette prime sera majorée de 100€ pour les salaries dont l'anciennete est superieure à 5 ans.
+		Ecrire l'algorithme qui permet de saisir le salaire et l'anciennete puis de calculer et d'afficher le montant de la prime 
+		d'un salarie.
 	*/
 	printf("--------------------------------------------\n");
 	printf("Voici le troisieme exercice du TD3, prime.\n\n");
@@ -56,21 +56,21 @@ int _tmain(int argc, _TCHAR* argv[])
 		prime += 100;
 	}
 
-	printf("Votre prime de fin d'annee est de %d euro\n", prime);
+	printf("Votre prime de fin d'annee est de %lf euro\n", prime);
 	printf("--------------------------------------------\n");
 	
 	/*
 		Exercice 4 : Train
-		A la gare Monparnasse, un voyageur s'arrête près d'un guichet automatique. Il souhaite connaitre le prix des billets
+		A la gare Monparnasse, un voyageur s'arrete près d'un guichet automatique. Il souhaite connaitre le prix des billets
 		pour aller dans les villes de Chartres, Angers et Nantes.
 		Voici les tarif :
 
-		- Chartes : 20.5 €
-		- Angers : 40 €
-		- Nantes : 50.5 €
+		- Chartes : 20.5 euro
+		- Angers : 40 euro
+		- Nantes : 50.5 euro
 
-		Ecrire l'algorithme qui affiche à l'écran le prix du billet correspondant à la ville saisie par le voyageur.
-		Affichage : "Un billet pour ... faut ... euro."
+		Ecrire l'algorithme qui affiche a l'ecran le prix du billet correspondant a la ville saisie par le voyageur.
+		Affichage : "Un billet pour ... vaut ... euro."
 	*/
 	printf("--------------------------------------------\n");
 	printf("Voici le quatrieme exercice du TD3, Train.\n\n");
@@ -81,28 +81,68 @@ int _tmain(int argc, _TCHAR* argv[])
 	double nantes = 50.5;
 
 	printf("Bienvenue a la gare de Monparnasse.\nSelectionnez votre destination :\n");
-	printf("1- Chartres : %d euro\n", chartres);
-	printf("2- Angers : %d euro\n", angers);
-	printf("3- Nantes : %d euro\n", nantes);
+	printf("1- Chartres : %lf euro\n", chartres);
+	printf("2- Angers : %lf euro\n", angers);
+	printf("3- Nantes : %lf euro\n", nantes);
 
 	scanf_s("%d",&choix);
 	
 	switch (choix)
 	{
 		case 1:
-			printf("un billet pour Chartres vaut %d euro.\n", chartres);
+			printf("un billet pour Chartres vaut %lf euro.\n", chartres);
 			break;
 
 		case 2:
-			printf("un billet pour Angers vaut %d euro.\n", angers);
+			printf("un billet pour Angers vaut %lf euro.\n", angers);
 			break;
 		case 3:
-			printf("un billet pour Nantes vaut %d euro.\n", nantes);
+			printf("un billet pour Nantes vaut %lf euro.\n", nantes);
 			break;
 		default:
 			printf("Vous devez choisir une destination.\n");
 			break;
 	}
+
+	printf("--------------------------------------------\n");
+
+	/*
+		Exercice 5 : Reduction
+		Une entreprise souhaite recompenser ses plus gros clients. Elle accorde une reduction en fonction du montant de la facture.
+		Cette reduction variable se calcule de la facon suiante :
+		- Pour un montant inférieur a 1 000 euro : pas de reduction.
+		- Pour un montant compris entre 1 000 euro et 10 000 euro : reduction de 5%.
+		- Pour un montant superieur a 10 000 euro : reduction de 10%.
+		Le directeur commercal vous demande de realiser un programme permettant de calculer le total que paiera le client 
+		(montant - reduction). 
+	*/
+
+	printf("--------------------------------------------\n");
+	printf("Voici le cinquieme exercice du TD3, Reduction.\n\n");
+
+	double montantFacture;
+	double montantAPayer;
+
+	printf("Entrez le montant de votre facture.\n");
+	scanf_s("%lf", &montantFacture);
+
+	if(montantFacture >= 10000)
+	{
+		montantAPayer = montantFacture - (montantFacture*0.1);
+	}
+	else 
+	{
+		if(montantFacture >= 1000)
+		{
+			montantAPayer = montantFacture - (montantFacture*0.05);
+		}
+		else
+		{
+			montantAPayer = montantFacture;
+		}
+	}
+
+	printf("Total a payer %lf euro\n", montantAPayer);
 
 	return 0;
 }
