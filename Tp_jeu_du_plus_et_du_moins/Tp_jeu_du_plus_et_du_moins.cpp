f34ab571548865de720e4d69487c50ac10410a68
Marie le Panda <lucas.girardin@outlook.com>
2013-05-20 02:56:43 +0200

    Tp du plus et du moins
    V1

---
+++ Tp_jeu_du_plus_et_du_moins/Tp_jeu_du_plus_et_du_moins.cpp
@@ -0,0 +1,57 @@
+// Tp_jeu_du_plus_et_du_moins.cpp : définit le point d'entrée pour l'application console.
+//
+	
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdafx.h"
+#include <cstdlib> 
+#include "time.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int nombreMystere;
+	int proposition;
+	int max = 100; 
+	int min = 0;
+	int nbCoups = 0;
+	int nbMaxCoups = 10;
+	bool trouve = false;
+
+	srand(time(NULL));
+	nombreMystere = (rand() %( max - min +1)) + min;
+
+	printf("Le nombre mystère a ete choisie. il est compris entre 0 et 100.\nVous avez 10 essaye pour le trouver.\n\nBonne chance !\n");
+	
+	do
+	{
+		printf("Dites un nombre : %d\n", nombreMystere);
+		scanf_s("%d", &proposition);
+		nbCoups++;
+
+		if( proposition < nombreMystere)
+		{
+			printf("C'est plus !\n");
+		}
+		else if(proposition > nombreMystere)
+		{
+			printf("C'est moins !\n");
+		}
+		else
+		{
+			trouve = true;
+		}
+
+	}while(!trouve && nbCoups < nbMaxCoups);
+
+	if (trouve)
+	{
+		printf("Feliciation le nombre mystere etait %d, vous l'avez trouve en %d coup(s) !\n", nombreMystere, nbCoups); 
+	}
+	else
+	{
+		printf("oh non ! Vous avez perdu !\n");
+	}
+
+	return 0;
+}
+
---
+++ Tp_jeu_du_plus_et_du_moins/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : fichier source incluant simplement les fichiers Include standard
+// Tp_jeu_du_plus_et_du_moins.pch représente l'en-tête précompilé
+// stdafx.obj contient les informations de type précompilées
+
+#include "stdafx.h"
+
+// TODO: faites référence aux en-têtes supplémentaires nécessaires dans STDAFX.H
+// absents de ce fichier
